8291d5906a2cfeb3680a008fb7c9ccffe5834ae6
Nikita Lipatov <6stringsohei@gmail.com>
2022-11-23 23:25:37 +0300

    Modified 2 actions: we are now getting access and refresh tokens in response payload; JWTPair and IRefreshToken are now in LimpShared Project.

---
+++ LimpShared/Authentification/IRefreshToken.cs
@@ -0,0 +1,9 @@
+﻿namespace LimpShared.Authentification
+{
+    public interface IRefreshToken
+    {
+        DateTime Created { get; set; }
+        DateTime Expires { get; set; }
+        string Token { get; set; }
+    }
+}
---
+++ LimpShared/Authentification/JWTPair.cs
@@ -0,0 +1,11 @@
+﻿#nullable disable
+using AuthAPI;
+
+namespace LimpShared.Authentification
+{
+    public class JWTPair
+    {
+        public string AccessToken { get; set; }
+        public IRefreshToken RefreshToken { get; set; }
+    }
+}
