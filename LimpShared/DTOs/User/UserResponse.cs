6e0d99a4cdedd6c99db6690e9e7f8e7de4f2da59
Nikita Lipatov <6stringsohei@gmail.com>
2023-02-02 22:08:09 +0300

    UserDTO is now wrapped in UserResponse class

---
+++ LimpShared/DTOs/User/UserResponse.cs
@@ -0,0 +1,9 @@
+﻿using AuthAPI.DTOs.User;
+
+namespace LimpShared.DTOs.User;
+
+public class UserResponse
+{
+    public string? SystemMessage { get; set; }
+    public UserDTO? UserDTO { get; set; }
+}
