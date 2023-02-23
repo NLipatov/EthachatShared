a71bae2547efce512b3f660692f98ea47d227454
Nikita Lipatov <6stringsohei@gmail.com>
2023-02-23 23:34:27 +0300

    AES keys are now stored with contact name

--- LimpShared/Encryption/Key.cs
+++ LimpShared/Encryption/Key.cs
@@ -5,4 +5,5 @@ public class Key
     public object? Value { get; set; }
     public KeyFormat? Format { get; set; }
     public KeyType? Type { get; set; }
+    public string? Contact { get; set; }
 }
