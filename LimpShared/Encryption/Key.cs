34fd0c6c7e66ee4562db1091466f8d3ba525bca4
Nikita Lipatov <6stringsohei@gmail.com>
2023-05-14 16:43:53 +0300

    Chat AES is now can be saved in local storage

--- LimpShared/Encryption/Key.cs
+++ LimpShared/Encryption/Key.cs
@@ -8,4 +8,5 @@ public class Key
     public string? Contact { get; set; }
     public string? Author { get; set; }
     public bool IsAccepted { get; set; } = false;
+    public DateTime CreationDate { get; set; } = DateTime.UtcNow;
 }
