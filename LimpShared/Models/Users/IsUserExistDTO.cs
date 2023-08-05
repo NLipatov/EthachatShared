f449bc6802323df43cf163630d6f90b9728d8fb6
Nikita Lipatov <6stringsohei@gmail.com>
2023-08-05 13:17:29 +0300

    Added a new model

---
+++ LimpShared/Models/Users/IsUserExistDTO.cs
@@ -0,0 +1,9 @@
+﻿#nullable disable
+namespace LimpShared.Models.Users
+{
+    public record IsUserExistDTO
+    {
+        public string Username { get; set; }
+        public bool IsExist { get; set; }
+    }
+}
