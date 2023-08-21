f017de5eaebd9a341d47eb2a3419100b63685175
Nikita Lipatov <6stringsohei@gmail.com>
2023-08-21 23:51:30 +0300

    added a RefreshTokenDTO model

---
+++ LimpShared/Models/Authentication/Models/RefreshTokenDTO.cs
@@ -0,0 +1,8 @@
+﻿#nullable disable
+namespace LimpShared.Models.Authentication.Models;
+
+public record RefreshTokenDTO
+{
+    public string UserAgent { get; set; }
+    public RefreshToken RefreshToken { get; set; }
+}
