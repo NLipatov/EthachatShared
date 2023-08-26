8933a22d3413a9fc53f20ac47ddce707966ce3f5
Nikita Lipatov <6stringsohei@gmail.com>
2023-08-26 14:42:06 +0300

    changed properties of the RefreshTokenDto

--- LimpShared/Models/Authentication/Models/RefreshTokenDTO.cs
+++ LimpShared/Models/Authentication/Models/RefreshTokenDTO.cs
@@ -3,6 +3,7 @@ namespace LimpShared.Models.Authentication.Models;
 
 public record RefreshTokenDto
 {
-    public Guid UserAgent { get; set; }
+    public string UserAgent { get; set; }
+    public Guid UserAgentId { get; set; }
     public RefreshToken RefreshToken { get; set; }
 }
