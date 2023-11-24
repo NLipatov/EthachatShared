a0c4a65d70b356d45aa7b71ba0b7a5f8a127e7b8
Nikita Lipatov <6stringsohei@gmail.com>
2023-11-24 14:39:53 +0400

    NotificationSubscriptionDTO.cs - replaced AccessToken with Credentials

--- LimpShared/Models/WebPushNotification/NotificationSubscriptionDTO.cs
+++ LimpShared/Models/WebPushNotification/NotificationSubscriptionDTO.cs
@@ -1,9 +1,11 @@
-﻿namespace LimpShared.Models.WebPushNotification
+﻿using LimpShared.Models.Authentication.Models.Credentials;
+
+namespace LimpShared.Models.WebPushNotification
 {
     public record NotificationSubscriptionDto
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public string? AccessToken { get; set; }
+        public ICredentials? Credentials { get; set; }
         public string? Url { get; set; }
         public string? P256dh { get; set; }
         public string? Auth { get; set; }
