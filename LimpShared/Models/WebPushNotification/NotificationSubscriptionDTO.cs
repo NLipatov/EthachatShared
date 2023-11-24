d007c0fedfb59584d1c63a83406fe1113617f8f9
Nikita Lipatov <6stringsohei@gmail.com>
2023-11-24 16:43:01 +0400

    added 2 props - JwtPair and WebAuthnPair instead of 1 prop - ICredentials

--- LimpShared/Models/WebPushNotification/NotificationSubscriptionDTO.cs
+++ LimpShared/Models/WebPushNotification/NotificationSubscriptionDTO.cs
@@ -1,11 +1,13 @@
 ﻿using LimpShared.Models.Authentication.Models.Credentials;
+using LimpShared.Models.Authentication.Models.Credentials.Implementation;
 
 namespace LimpShared.Models.WebPushNotification
 {
     public record NotificationSubscriptionDto
     {
         public Guid Id { get; set; } = Guid.NewGuid();
-        public ICredentials? Credentials { get; set; }
+        public JwtPair? JwtPair { get; set; }
+        public WebAuthnPair? WebAuthnPair { get; set; }
         public string? Url { get; set; }
         public string? P256dh { get; set; }
         public string? Auth { get; set; }
