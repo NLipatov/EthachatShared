f9607dc640049b6ac9c5222b0cefa91961d5a6e3
Nikita Lipatov <6stringsohei@gmail.com>
2023-11-28 14:50:17 +0400

    added CredentialsDTO

---
+++ LimpShared/Models/Authentication/Models/Credentials/CredentialsDTO/CredentialsDTO.cs
@@ -0,0 +1,9 @@
+﻿using LimpShared.Models.Authentication.Models.Credentials.Implementation;
+
+namespace LimpShared.Models.Authentication.Models.Credentials.CredentialsDTO;
+
+public record CredentialsDTO
+{
+    public JwtPair? JwtPair { get; set; }
+    public WebAuthnPair? WebAuthnPair { get; set; }
+}
