d28fb6e274efa2a5b6a843e625524ba868080206
Nikita Lipatov <6stringsohei@gmail.com>
2023-12-03 16:29:59 +0400

    added IV string to a Package, as package B64Data now contains a encrypted binary file data Base64 string

--- LimpShared/Models/Message/DataTransfer/Package.cs
+++ LimpShared/Models/Message/DataTransfer/Package.cs
@@ -9,4 +9,5 @@ public record Package
     public string B64Data { get; set; }
     public string ContentType { get; set; }
     public string FileName { get; set; }
+    public string IV { get; set; }
 }
