de28c83ab3b8edf2ec56872538935071dc55ddd0
Nikita Lipatov <6stringsohei@gmail.com>
2024-02-11 15:20:52 +0400

    added a total property to Package.cs

--- EthachatShared/Models/Message/DataTransfer/Package.cs
+++ EthachatShared/Models/Message/DataTransfer/Package.cs
@@ -5,6 +5,7 @@ public record Package
 {
     public Guid FileDataid { get; set; }
     public int Index { get; set; }
+    public int Total { get; set; }
     public string B64Data { get; set; }
     public string IV { get; set; }
 }
