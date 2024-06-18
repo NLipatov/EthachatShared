3031442d2705eef327f36b6a75e3f59869af3234
Nikita Lipatov <6stringsohei@gmail.com>
2024-06-18 20:46:45 +0300

    Text Message now implements IDestinationResolvable

--- EthachatShared/Models/Message/ClientToClientTransferData/TextMessage.cs
+++ EthachatShared/Models/Message/ClientToClientTransferData/TextMessage.cs
@@ -2,12 +2,12 @@ using EthachatShared.Models.Message.Interfaces;
 
 namespace EthachatShared.Models.Message.ClientToClientTransferData;
 
-public record TextMessage : IIdentifiable
+public record TextMessage : IIdentifiable, IDestinationResolvable
 {
     public Guid Id { get; set; } = Guid.NewGuid();
     public required string Sender { get; set; }
-    public required string Receiver { get; set; }
     public string Text { get; set; } = string.Empty;
     public int Index { get; set; } = 0;
     public int Total { get; set; } = 1;
+    public string Target { get; set; }
 }
