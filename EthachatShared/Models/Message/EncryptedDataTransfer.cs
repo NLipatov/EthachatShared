01cb35d77e1d08ba1b53c32f2310f91b08b44a8f
Nikita Lipatov <6stringsohei@gmail.com>
2024-06-19 15:11:08 +0300

    model update

--- EthachatShared/Models/Message/EncryptedDataTransfer.cs
+++ EthachatShared/Models/Message/EncryptedDataTransfer.cs
@@ -3,7 +3,7 @@ using EthachatShared.Models.Message.Interfaces;
 
 namespace EthachatShared.Models.Message;
 
-public record EncryptedDataTransfer : IDestinationResolvable, ISourceResolvable, IDescribeable, IIdentifiable
+public record EncryptedDataTransfer : IDestinationResolvable, ISourceResolvable, IDescribeable, IIdentifiable, IHasInnerDataType
 {
     public required Guid Id { get; set; } = Guid.NewGuid();
     public required Cryptogram Cryptogram { get; set; }
---
+++ EthachatShared/Models/Message/Interfaces/IHasInnerDataType.cs
@@ -0,0 +1,6 @@
+namespace EthachatShared.Models.Message.Interfaces;
+
+public interface IHasInnerDataType
+{
+    public Type DataType { get; set; }
+}
