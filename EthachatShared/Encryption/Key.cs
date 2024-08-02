0f3411e6aae600ff73496b8bba2b1b5ed695a96f
Nikita <6stringsohei@gmail.com>
2024-08-02 20:10:46 +0300

    Key minor update

--- EthachatShared/Encryption/Key.cs
+++ EthachatShared/Encryption/Key.cs
@@ -6,11 +6,11 @@ namespace EthachatShared.Encryption;
 public class Key
 {
     [Key(0)] public required Guid Id { get; init; }
-    [Key(1)] public string Value { get; set; }
-    [Key(2)] public KeyFormat? Format { get; set; }
-    [Key(3)] public KeyType? Type { get; set; }
+    [Key(1)] public required string Value { get; init; }
+    [Key(2)] public KeyFormat? Format { get; init; }
+    [Key(3)] public KeyType? Type { get; init; }
     [Key(4)] public string? Contact { get; set; }
     [Key(5)] public string? Author { get; set; }
-    [Key(6)] public bool IsAccepted { get; set; } = false;
-    [Key(7)] public DateTime CreationDate { get; set; } = DateTime.UtcNow;
+    [Key(6)] public bool IsAccepted { get; set; }
+    [Key(7)] public DateTime CreationDate { get; init; } = DateTime.UtcNow;
 }
