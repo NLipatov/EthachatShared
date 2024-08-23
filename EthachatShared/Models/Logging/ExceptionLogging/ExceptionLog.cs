bba4250411198ef3f0e5d533a470dd7e01ad2128
Nikita <6stringsohei@gmail.com>
2024-08-23 17:55:15 +0300

    Model updates

--- EthachatShared/Models/Logging/ExceptionLogging/ExceptionLog.cs
+++ EthachatShared/Models/Logging/ExceptionLogging/ExceptionLog.cs
@@ -1,6 +1,10 @@
+using MessagePack;
+
 namespace EthachatShared.Models.Logging.ExceptionLogging;
 
+[MessagePackObject]
 public record ExceptionLog : Log
 {
+    [Key(0)]
     public string? StackTrace { get; set; }
 }
--- EthachatShared/Models/WebPushNotification/NotificationSubscriptionDTO.cs
+++ EthachatShared/Models/WebPushNotification/NotificationSubscriptionDTO.cs
@@ -1,16 +1,26 @@
 ﻿using EthachatShared.Models.Authentication.Models.Credentials.Implementation;
+using MessagePack;
 
 namespace EthachatShared.Models.WebPushNotification
 {
+    [MessagePackObject]
     public record NotificationSubscriptionDto
     {
+        [Key(0)]
         public Guid Id { get; set; } = Guid.NewGuid();
+        [Key(1)]
         public JwtPair? JwtPair { get; set; }
+        [Key(2)]
         public WebAuthnPair? WebAuthnPair { get; set; }
+        [Key(3)]
         public string? Url { get; set; }
+        [Key(4)]
         public string? P256dh { get; set; }
+        [Key(5)]
         public string? Auth { get; set; }
+        [Key(6)]
         public Guid UserAgentId { get; set; }
+        [Key(7)]
         public string? FirebaseRegistrationToken { get; set; }
     }
 }
